fc674f5b860e7055bdab53fcf365557ddf66a4e1
Kevin <chenning.0515@gmail.com>
2017-07-14 14:42:36 +1200

    Initial vue project

---
+++ src/components/Index.vue
@@ -0,0 +1,40 @@
+
+<template>
+<div>
+	<h1 class="Logo"> Vue api test </h1>
+	<ul class="List">
+		<li v-for="item in lists" v-text = "item.title"></li>
+	</ul>
+</div>
+</template>
+
+<script>
+export default {
+	data(){
+		return {
+			lists:[]
+		}
+	},
+	created(){
+		this.get_data()
+	},
+	methods:{
+		get_data :function(params){
+			if(!params) params = {};
+			this.$http.get('https://cnodejs.org/api/v1/topics').then(function(response){
+				this.lists = response.data.data;
+				console.log(response.data.data)
+			});
+		}
+	}
+}
+</script>
+
+<style scoped>
+.logo {color: red;}
+
+.list {
+  line-height: 2;
+  li {border-bottom: 1px solid #ddd;}
+}
+</style>
